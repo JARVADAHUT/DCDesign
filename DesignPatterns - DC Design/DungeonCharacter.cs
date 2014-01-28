2f2d3a8195a959df3f710cbd6a5791ee944c67c5
Andrew McCall <mccall.andrew@gmail.com>
2014-01-28 11:12:47 -0800

    First Push

---
+++ DesignPatterns - DC Design/DungeonCharacter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns___DC_Design
+{
+    abstract class DungeonCharacter
+    {
+        int _maxHP, _curHp;
+        int _maxResources, _curResources;
+        GearSet _gearInfo;
+        string name;
+
+        List<ICombatAction> combatActions;
+    }
+}
